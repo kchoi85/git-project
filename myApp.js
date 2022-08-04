63ef46e212ac8a761f1188e2c85514a8981a8ae4
David Choi <davidchoi0304@gmail.com>
2022-08-04 11:45:50 -0400

    Added myApp.js file

---
+++ myApp.js
@@ -0,0 +1,11 @@
+// 2022 August Git Course
+
+const myApp = {
+  name: "My App",
+  version: "1.0.0",
+  init: function () {
+    console.log(`${this.name} ${this.version}`);
+  },
+};
+
+myApp.init();
